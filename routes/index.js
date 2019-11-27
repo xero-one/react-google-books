278d3eb38fc152468737b71321ddab8c188b460a
Chad Gresham <bxxdybagz@gmail.com>
2019-11-26 23:14:30 -0500

    added index.js file for the routes folder

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+/* Here is the body api or "index" for the routing dir*/
+
+const path = require("path");
+
+/*Set express to a constant global variable and init the npm express to run with this route body javascript code*/
+const router = require("express").Router();
+
+/*Set constant variable to call all api information from directory at "/api"*/
+const apiRoutes = require("./api");
+
+/*api routes*/
+router.use("/api", apiRoutes);
+
+// default - if no routs hit send home page (*)
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+
+/*"module.exports" export the route so it's available in other parts of the app*/
+module.exports = router;
