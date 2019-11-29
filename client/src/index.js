b00abcd22818c82b5d5f0b1ac7c3f6b44b46b1bb
Chad Gresham <bxxdybagz@gmail.com>
2019-11-28 23:36:41 -0500

    deploy deploy deploy

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import registerServiceWorker from "./registerServiceWorker";
 
-
-
-registerServiceWorker();
 ReactDOM.render(<App />, document.getElementById("root"));
