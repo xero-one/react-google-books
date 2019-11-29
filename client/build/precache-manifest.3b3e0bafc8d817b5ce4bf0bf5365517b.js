20616dae4534084991855cc0b92da24ef9a9da6a
Chad Gresham <bxxdybagz@gmail.com>
2019-11-29 05:47:21 -0500

    deploy deploy deploy

---
+++ client/build/precache-manifest.3b3e0bafc8d817b5ce4bf0bf5365517b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84137b5a40fb3eca680fc8daa49d5d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f26458ccc1b5ae244aa",
+    "url": "/static/css/main.a770943f.chunk.css"
+  },
+  {
+    "revision": "47dfe0866304c838a145",
+    "url": "/static/js/2.59a3bfa6.chunk.js"
+  },
+  {
+    "revision": "3f26458ccc1b5ae244aa",
+    "url": "/static/js/main.d91ec37c.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
